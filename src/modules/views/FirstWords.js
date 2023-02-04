9299a65c3ce6004c67f5fa3a51ecba413eb7e248
alanmcginnis <7727219+alanmcginnis@users.noreply.github.com>
2023-02-04 15:18:05 -0800

    fix text alignment issue

--- src/modules/views/FirstWords.js
+++ src/modules/views/FirstWords.js
@@ -25,6 +25,7 @@ function BirthStats() {
             border: "4px solid currentColor",
             borderRadius: 0,
             height: "auto",
+            textAlign: "center",
             py: 2,
             px: 5,
             mb: 6,
