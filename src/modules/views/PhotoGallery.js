196962a1ac5866aeaaf9f38ade03b77427eb4cf6
alanmcginnis <7727219+alanmcginnis@users.noreply.github.com>
2023-02-03 22:27:31 -0800

    Workable

--- src/modules/views/PhotoGallery.js
+++ src/modules/views/PhotoGallery.js
@@ -1,5 +1,6 @@
 import Fancybox from "../components/Fancybox";
 import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
 import Masonry from "@mui/lab/Masonry";
 
 const settings = {
@@ -11,41 +12,64 @@ const settings = {
 
 function PhotoGallery() {
   return (
-    <Box sx={{ width: "80%", maxWidth: 700, minHeight: 829 }}>
-      <Masonry columns={{ xs: 2, sm: 3, md: 4 }} spacing={2}>
-        <Fancybox options={{ infinite: false }}>
-          {[...Array(40)].map((e, i) => {
-            let thumbnailImg = `${settings.thumbnailURL}group-${settings.group}Francis-${i}.jpg`;
-            let img = `${settings.baseURL}group-${settings.group}Francis-${i}.jpg`;
+    <Box
+      className="photo-gallery"
+      component="section"
+      sx={{ display: "flex", overflow: "hidden" }}
+    >
+      <Container
+        sx={{
+          mt: 8,
+          mb: 8,
+          display: "flex",
+          flexWrap: "wrap",
+          position: "relative",
+          justifyContent: "center",
+        }}
+      >
+        <Box
+          sx={{
+            width: "80%",
+            maxWidth: 700,
+            minHeight: 829,
+          }}
+        >
+          <Masonry columns={{ xs: 2, sm: 3, md: 4 }} spacing={2}>
+            <Fancybox options={{ infinite: false }}>
+              {[...Array(40)].map((e, i) => {
+                let thumbnailImg = `${settings.thumbnailURL}group-${settings.group}Francis-${i}.jpg`;
+                let img = `${settings.baseURL}group-${settings.group}Francis-${i}.jpg`;
 
-            if (i === 0) {
-              return null;
-            }
+                if (i === 0) {
+                  return null;
+                }
 
-            if (i < 10) {
-              thumbnailImg = `${settings.thumbnailURL}group-${settings.group}/Francis-0${i}.jpg`;
-              img = `${settings.baseURL}group-${settings.group}/Francis-0${i}.jpg`;
-            }
+                if (i < 10) {
+                  thumbnailImg = `${settings.thumbnailURL}group-${settings.group}/Francis-0${i}.jpg`;
+                  img = `${settings.baseURL}group-${settings.group}/Francis-0${i}.jpg`;
+                }
 
-            return (
-              <a key={i} data-fancybox="gallery" href={`${img}`}>
-                <img
-                  src={`${thumbnailImg}`}
-                  srcSet={`${thumbnailImg}`}
-                  alt="Sweet Baby Francis"
-                  loading="lazy"
-                  style={{
-                    borderBottomLeftRadius: 4,
-                    borderBottomRightRadius: 4,
-                    display: "block",
-                    width: "100%",
-                  }}
-                />
-              </a>
-            );
-          })}
-        </Fancybox>
-      </Masonry>
+                return (
+                  <a key={i} data-fancybox="gallery" href={`${img}`}>
+                    <img
+                      src={`${thumbnailImg}`}
+                      srcSet={`${thumbnailImg}`}
+                      alt="Sweet Baby Francis"
+                      loading="lazy"
+                      style={{
+                        borderBottomLeftRadius: 4,
+                        borderBottomRightRadius: 4,
+                        display: "block",
+                        width: "100%",
+                      }}
+                    />
+                  </a>
+                );
+              })}
+            </Fancybox>
+          </Masonry>
+        </Box>
+      </Container>
     </Box>
   );
 }
